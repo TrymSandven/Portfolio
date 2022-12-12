b7a465f6c81c55d65a0150d7e7503893e6617459
TrymSandven <64443177+TrymSandven@users.noreply.github.com>
2022-12-12 18:54:51 +0000

    updated about page

--- src/App.jsx
+++ src/App.jsx
@@ -24,12 +24,12 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
+  name: "Trym Sandven",
+  title: "Python utviklar og maskinlæring",
+  email: "trymsandven@gmail.com",
+  gitHub: "TrymSandven",
   instagram: "microsoft",
-  linkedIn: "satyanadella",
+  linkedIn: "trym-sandven-29200522b",
   medium: "",
   twitter: "microsoft",
   youTube: "Code",
--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -24,19 +24,19 @@ const imageAltText = "purple and blue abstract background";
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "Eg er ein nyutdanna utviklar frå UiB med interesse for maskinlæring og webutvikling.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
+  "Python",
+  "SQL",
+  "Maskinlæring",
+  "Web utvikling",
+  "Web scraping",
+  "API arbeid",
 ];
 
 /**
@@ -45,7 +45,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  " Eg har erfaring med å jobbe med data og å lage nettsider. Eg er ein god teamspelar som likar å jobbe med andre. Eg er fleksibel og likar å lære nye ting.";
 
 const About = () => {
   return (
@@ -60,7 +60,7 @@ const About = () => {
           textAlign: "center",
         }}
       >
-        <h2>About Myself</h2>
+        <h2>Om meg sjølv</h2>
         <p className="large">{description}</p>
         <hr />
         <ul
