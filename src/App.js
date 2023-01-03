5373f43b201ca44b43b8cbc9aa1aff15390942fc
TrymSandven <64443177+TrymSandven@users.noreply.github.com>
2023-01-03 16:20:05 +0100

    Ny Portfolie side

---
+++ src/App.js
@@ -0,0 +1,29 @@
+
+import './App.css';
+import Header from './components/header/Header';
+import Navbar from './components/navbar/Navbar';
+import Features from './components/features/Features';
+import Download from './components/download/Download';
+import Faq from './components/faq/Faq';
+import Subscribe from './components/subscribe/Subscribe';
+import Footer from './components/footer/Footer';
+
+function App() {
+  return (
+    <main>
+      <header className='header-bg'>
+        <Navbar/>
+        <Header />
+      </header>
+      <Features />
+      <Download />
+      <Subscribe />
+      <Faq />
+      <Footer />
+
+      
+    </main>
+  );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
-/**
- * Entry point of application, where App is rendered within the div with the id of "app" 
- */
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
 
-import React from "react";
-import { render } from "react-dom";
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
-import App from "./App";
-
-render(<App></App>, document.getElementById("app"));
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
