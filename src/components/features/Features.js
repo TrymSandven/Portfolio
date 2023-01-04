e6fce144a912f818210a2244d8b85df17c0f6f26
TrymSandven <64443177+TrymSandven@users.noreply.github.com>
2023-01-04 13:03:36 +0100

    Lagt til rammeverket for om meg

--- src/components/features/Features.js
+++ src/components/features/Features.js
@@ -2,8 +2,25 @@ import React from 'react'
 
 function Features() {
   return (
-    <div>
-      <h1>Om meg</h1>
+    <section id='features'>
+      <div className='container features'>
+        <div className='u-title'>
+          <BsFillBookmarkStarFill color='#FF4500' size={30}/>
+          <h2>Om meg</h2>
+          <p className='u-text-small u-text-dark'>Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum </p>
+        </div>
+        <div className='features-content'>
+          <div className='features-left'>
+            <img src={bilde} alt='bilde av meg'/>
+          <div/>
+          <div className='features-right'>
+            {
+              featureList.map((feature) => (
+                <Feature key={feature.id} icon={feature.icon} heading={feature.heading} text={feature.text}/>
+              ))            
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
